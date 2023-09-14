8e3d91dd4fb2315456ccc32beb2226ea1e1e03d6
jacobberry <70248350+bobedablaine@users.noreply.github.com>
2023-09-13 23:07:54 -0700

    Update back.py
    For 1.1 if he checks

--- back.py
+++ back.py
@@ -1,7 +1,6 @@
 print("Welcome to Currency Convertor")
 print("Please enter your starting currency")
-currencyA = input()
-print("Please enter your ending currency")
-currencyB = input()
-print("Please enter the amount")
-amount = input()
+currencyA = float(input())
+print("Default will be from USD to EUR")
+currencyA *= 0.93 
+print("Result: ", currencyA)

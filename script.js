cb5ca0cf4234b33b067b44759e99c6e830e6d348
PrinceSidon <siddharth.vasu.ca@gmail.com>
2023-09-24 13:20:55 -0700

    Fixed dropdown issues

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 // Function to fetch currency data from the OpenExchangeRates API
-function fetchCurrencies(id) {
-    // Replace 'YOUR_API_KEY' with your actual API key
+async function fetchCurrencies(id) {
     const apiKey = 'bf69802c79904002ba1d2b435771995f';
     const apiUrl = `https://openexchangerates.org/api/currencies.json?app_id=${apiKey}`;
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Get the select element
-            const currencySelect = document.getElementById(id);
+    const response = await fetch(apiUrl);
+    const currencies = await response.json();
 
-            // Loop through the currency data and add options to the select element
-            for (const currencyCode in data) {
-                if (currencyCode != "USD") {
-                    const option = document.createElement('option');
-                    option.value = currencyCode;
-                    option.text = `${currencyCode} - ${data[currencyCode]}`;
-                    currencySelect.appendChild(option);
-                }
-            }
-        })
-        .catch(error => console.error('Error fetching currency data:', error));
+    const currencySelect = document.getElementById(id);
+
+    for (const currency in currencies) {
+        if (currency != "USD") {
+            const option = document.createElement('option');
+            option.value = currency;
+            option.text = `${currency} - ${currencies[currency]}`;
+            currencySelect.appendChild(option);
+        }
+    }
 }

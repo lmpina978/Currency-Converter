8fe33afed57c85c9cb85d289aac3becaded8b29d
Jacob Berry <jacobberry777@yahoo.com>
2023-09-25 20:27:16 -0700

    Updated Python file and url for css and js

--- back.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to Currency Convertor")
-print("Please enter your starting currency")
-currencyA = float(input())
-print("Default will be from USD to EUR")
-currencyA *= 0.93 
-print("Result: ", currencyA)

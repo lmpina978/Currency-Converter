b22001d9f627647cc3852579cad7cbc2de11e35a
jacobberry <70248350+bobedablaine@users.noreply.github.com>
2023-09-27 21:23:53 -0700

    Update script.js
    Added the calculations to the js file

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
+const apiKey = 'bf69802c79904002ba1d2b435771995f';
+
 // Function to fetch currency data from the OpenExchangeRates API
 async function fetchCurrencies(id) {
-    const apiKey = 'bf69802c79904002ba1d2b435771995f';
     const apiUrl = `https://openexchangerates.org/api/currencies.json?app_id=${apiKey}`;
 
     const response = await fetch(apiUrl);
@@ -16,4 +17,46 @@ async function fetchCurrencies(id) {
             currencySelect.appendChild(option);
         }
     }
+}
+
+async function currencyCalculations(currencyA, currencyB, amount)
+{
+    const curUrl = `https://openexchangerates.org/api/latest.json?app_id=${apiKey}`;
+    //const apiUrl = `https://openexchangerates.org/api/currencies.json?app_id=${apiKey}`;
+
+    const response = await fetch(curUrl);
+    const currencies = await response.json();
+
+    //const curA_name = document.querySelector(currencyA);
+    const curA_name = document.getElementById(currencyA);
+    //Sconsole.log(curA_name.value);
+    var output = curA_name.value;
+
+    //const curB_name = document.querySelector(currencyB);
+    const curB_name = document.getElementById(currencyB);
+    //console.log(curB_name.id);
+
+    var output2 = curB_name.value;
+
+    var userAmount = document.getElementById(amount).value;
+    console.log(userAmount);
+    var rateA;
+    var rateB;
+    for (const currency in currencies.rates) {
+        //console.log(currency);
+        if (currency == curA_name.value) {
+            rateA = currencies.rates[currency];
+            //console.log(rateA); 
+        }
+        
+        if (currency == curB_name.value) {
+            rateB = currencies.rates[currency];
+        }
+    }
+    console.log(rateA);
+    console.log(rateB);
+    var result = userAmount * (rateB / rateA);
+
+    console.log(result);
+
 }

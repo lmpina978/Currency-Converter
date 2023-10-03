68c5094c65ba50a2a9e62f7dabfabc894fc20766
PrinceSidon <siddharth.vasu.ca@gmail.com>
2023-10-02 22:51:40 -0700

    Finished convert function

--- script.js
+++ script.js
@@ -19,44 +19,36 @@ async function fetchCurrencies(id) {
     }
 }
 
-async function currencyCalculations(currencyA, currencyB, amount)
+async function currencyCalculations(currencyA, currencyB, amount, display)
 {
     const curUrl = `https://openexchangerates.org/api/latest.json?app_id=${apiKey}`;
-    //const apiUrl = `https://openexchangerates.org/api/currencies.json?app_id=${apiKey}`;
 
-    const response = await fetch(curUrl);
-    const currencies = await response.json();
+    const curResponse = await fetch(curUrl);
+    const currencies = await curResponse.json();
 
-    //const curA_name = document.querySelector(currencyA);
     const curA_name = document.getElementById(currencyA);
-    //Sconsole.log(curA_name.value);
-    var output = curA_name.value;
-
-    //const curB_name = document.querySelector(currencyB);
     const curB_name = document.getElementById(currencyB);
-    //console.log(curB_name.id);
-
-    var output2 = curB_name.value;
 
     var userAmount = document.getElementById(amount).value;
-    console.log(userAmount);
-    var rateA;
-    var rateB;
+
     for (const currency in currencies.rates) {
-        //console.log(currency);
         if (currency == curA_name.value) {
-            rateA = currencies.rates[currency];
-            //console.log(rateA); 
+            var rateA = currencies.rates[currency]; 
         }
         
         if (currency == curB_name.value) {
-            rateB = currencies.rates[currency];
+            var rateB = currencies.rates[currency];
         }
     }
-    console.log(rateA);
-    console.log(rateB);
+
     var result = userAmount * (rateB / rateA);
+    if (result > 0.01) {
+        result = result.toFixed(2);
+    }
+    else {
+        result = result.toPrecision(2);
+    }
 
-    console.log(result);
-
+    display = document.getElementById(display);
+    display.innerText = result + " " + curB_name.value;
 }
